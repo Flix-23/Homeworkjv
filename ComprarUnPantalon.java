2a94bc150e093661d9375a188482377d7e5faded
Flix-23 <123584809+Flix-23@users.noreply.github.com>
2023-03-20 23:48:18 -0400

    compra de pantalor y tomar vaso de agua

---
+++ ComprarUnPantalon.java
@@ -0,0 +1,13 @@
+public class ComprarUnPantalon{ 
+   public static void main(String[] args){
+
+   System.out.println("Me dirijo a una tienda donde vendan pantalones");
+   System.out.println("Ubico el area de pantalones");
+   System.out.println("Me dirijo al area de pantalones de mi gusto");
+   System.out.println("Busco un pantalon de mi talla");
+   System.out.println("Me dirijo al vestidor");
+   System.out.println("Compruebo de que me queda perfectamente como quiero");
+   System.out.println("Me dirijo a caja a realizar el pago del pantalon");
+}
+
+}
